2e91f3771fc3953bb8eae70b6ca80a199a0dc74f
ropenfold <stuartroper@gmail.com>
2019-04-15 16:48:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:8080", { transports: ["websocket"] });
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      input: ""
+    };
+  }
+
+  componentDidMount() {
+    socket.on("chat message", msg => {
+      this.setState(
+        state => ({
+          messages: [...state.messages, msg]
+        }),
+        () => {
+          window.scrollTo(0, document.body.scrollHeight);
+        }
+      );
+    });
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState(() => ({
+      input: value
+    }));
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("SUBMITTING FORM");
+    socket.emit("chat message", this.state.input);
+    this.setState(() => ({
+      input: ""
+    }));
+  };
+
   render() {
+    const { messages, input } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {messages.map((message, idx) => (
+          <li key={idx}>{message}</li>
+        ))}
+        <form onSubmit={this.handleSubmit}>
+          <input value={input} onChange={this.handleChange} />
+          <button>Send</button>
+        </form>
       </div>
     );
   }
